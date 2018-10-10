3ab95ea4760db1ed90142cb7eb59cd5a71718efd
Joshua anderson <Mp4ct@live.com>
2018-10-10 08:36:39 -0500

    Added more Bluetooth support

--- app/src/main/java/com/example/surfacejosh/myapplication/SeniorProject.java
+++ app/src/main/java/com/example/surfacejosh/myapplication/SeniorProject.java
@@ -1,75 +1,106 @@
 package com.example.surfacejosh.myapplication;
 
-import com.physicaloid.lib.usb.driver.uart.ReadLisener;
-import com.physicaloid.lib.Physicaloid;
-
 import android.bluetooth.BluetoothAdapter;
+//import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.os.Handler;
+//import android.widget.TextView;
+
+
 
 public class SeniorProject extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     //Create UI elements and lib variables
-    Button SYNC, MAF_WORKOUT, BT_SEARCH;
-    TextView tvRead;
+    //Button SYNC, MAF_WORKOUT, BT_SEARCH;
+    //TextView tvRead;
     BluetoothAdapter mBluetoothadapter;
-    Physicaloid mPhysicaloid;
+
     ///////////////////////////////////
-
-
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(mBluetoothadapter.ACTION_STATE_CHANGED)){
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothadapter.ERROR);
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onRecieve: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "onRecieve: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "onRecieve: STATE ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "onRecieve: STATE TURNING ON");
+                        break;
+                }
+            //when discovery finds a device
+            //if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                //BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //String deviceName = device.getName();
+                //String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+    @Override
+    protected void onDestroy(){
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver1);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senior_project);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         Button BT_SEARCH = (Button) findViewById(R.id.BT_SEARCH);
+        mBluetoothadapter = BluetoothAdapter.getDefaultAdapter();
+
         BT_SEARCH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onClickBTSEARCH(view);
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                     //   .setAction("Action", null).show();
+                Log.d(TAG, "onClick: Enabling Bluetooth. ");
+                enableDisableBT();
+
             }
         });
 
-
-        // in onCreate Method Initialize The variables
-       // BT_SEARCH = (Button) findViewById(R.id.BT_SEARCH);
-        SYNC = (Button) findViewById(R.id.SYNC);
-        MAF_WORKOUT  = (Button) findViewById(R.id.MAF_WORKOUT);
-        tvRead = (TextView) findViewById(R.id.TextV);
-
-        mBluetoothadapter = BluetoothAdapter.getDefaultAdapter();
-        mPhysicaloid = new Physicaloid(this);
-        ///////////////////////////
-
     }
 
-    public void onClickBTSEARCH(View v) {
-        mPhysicaloid.setBaudrate(9600);
-        mPhysicaloid.addReadListener(new ReadLisener()
-        {
-            int size ;
-                public void onRead(int size) {
-                    byte[] buf = new byte[size];
-                    mPhysicaloid.read(buf, size);
-                    tvAttatch(tvRead,("<font color=blue>" + new String(buf) + "</font>"));
-                }
+    public void enableDisableBT() {
+        if (mBluetoothadapter == null) {
+            Log.d(TAG, "Device cannot recieve BT");
+        }
+        if (!mBluetoothadapter.isEnabled()) {
+            Log.d(TAG, "enableDisableBT: Enabling BT.");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
 
-        });
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1,BTIntent);
+        }
+        if(mBluetoothadapter.isEnabled()){
+            Log.d(TAG, "enableDisable: Disable BT.");
+            mBluetoothadapter.disable();
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1,BTIntent);
+        }
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -94,9 +125,9 @@ public class SeniorProject extends AppCompatActivity {
     }
 
     //// Handles Text from Arduino
-    Handler mHandle = new Handler();
-    private void tvAttatch(TextView tv, CharSequence text)
-    {
+    //Handler mHandle = new Handler();
+/*
+    private void tvAttatch(TextView tv, CharSequence text) {
         final TextView ftv = tv;
         final CharSequence ftext = text;
         mHandle.post(new Runnable() {
@@ -107,4 +138,5 @@ public class SeniorProject extends AppCompatActivity {
         });
     }
     /////////////////
+    */
 }
